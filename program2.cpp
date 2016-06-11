72336b6952c8d59f379dc11b55252c7624a51e64
Keith <keithpotz@gmail.com>
2016-06-11 13:23:20 -0600

    Update program2.cpp

--- program2.cpp
+++ program2.cpp
@@ -1,7 +1,7 @@
-// Keith Pottratz
-// September 14, 2014
-// This Program is designed to output hours work taxes and total gross income
-// of all employees and categorizing them into Full or Part-time
+/*Keith Pottratz
+September 14, 2014
+ This Program is designed to output hours work taxes and total gross income
+ of all employees and categorizing them into Full or Part-time*/
 #include <iostream>
 #include <fstream>
 #include <string>
