65ccb66830f02ea46ecf1b097f091791cd219888
Keith <keithpotz@gmail.com>
2023-07-18 23:58:34 -0400

    Update
    Updated and epitomized this code. Changed "using namespace std" to std:: in order to specify verses using global naming. Got rid of string copying for 'name' variable. Mainly because it's already cleared and doesn't need to be iterated again.

--- program2.cpp
+++ program2.cpp
@@ -6,74 +6,57 @@ September 14, 2014
 #include <fstream>
 #include <string>
 #include <iomanip>
-using namespace std;
-void main()
+
+int main()
 {
-	string name;
-	int i, hoursw;
-	double hourr, gross, tax, net, totaltax = 0, totalhw = 0, totalg = 0,totalnet=0;
-	char Emplyt;
-	ifstream inf;
-	ofstream outf;
-	inf.open("program2.dat");
-	outf.open("program2.ot");
-	outf.setf(ios::fixed);
-	outf.precision(2); 
-	outf << endl;
-	outf << setw(47) << "Employee Pay Table" << endl;
-	outf << setw(43) << "Fall, 2014" << endl; outf << endl;
-	outf << setw(1) << "Employee" << setw(14) << "Employee" 
-		<< setw(10) << "Hours" << setw(11) << "Hourly" << setw(9) << "Gross"
-		<< endl;
-	outf << setw(5) << "Name" << setw(14) << "Type" << setw(14)
-		<< "Worked" << setw(9) << "Rate" << setw(11) << "Salary"
-		<< setw(10) << "Taxes" << setw(7) << "Net" << endl;
-	for (i = 1; i <= 73; i++) outf << "-";
-	outf << endl;
-	for (i = 1; i <= 10; i++)
-	{
-		name = "";
-		hourr = 0;
-		hoursw = 0;
-		inf >> name >> Emplyt >> hourr >> hoursw;
-		gross = 0;
-		if (hoursw > 40)
-		{
-			gross = (hoursw - 40)*(hourr*1.5) + (40 * hourr);
-		}
-		else
-		{
-			gross = hourr * hoursw;
-		}
-		if (Emplyt == 'F')
-		{
-			if (gross < 250)
-				tax = 0;
-			else if (gross < 400)
-				tax = gross * .15;
-			else
-				tax = gross * .25;
-		}
-		else {
-			if (gross >= 250)
-				tax = gross *.2;
-			else if (gross >= 125)
-				tax = gross *.1;
-			else
-				tax = 0;
-		}
-		net = gross - tax;
-		totalhw += hoursw;
-		totaltax += tax;
-		totalnet += net;
-		totalg += gross;
-		
-		outf << setw(15) <<left << name << setw(10) << Emplyt << setw(5) << right << hoursw << setw(12)
-			<< hourr << setw(10) << gross << setw(10) << tax << setw(10)  << net << endl;
-		}
-	for (i = 1; i <= 73; i++) outf << "-";
-	outf << endl;
-	outf << setw(32) << totalhw <<setw(20) << totalg << setw(10)<< totaltax << setw(11) << totalnet << endl;
-	system("pause");
-	}
+    std::string name;
+    int i, hoursw;
+    double hourr, gross, tax, net, totaltax = 0, totalhw = 0, totalg = 0, totalnet = 0;
+    char Emplyt;
+
+    std::ifstream inf("program2.dat");
+    std::ofstream outf("program2.ot");
+    outf.setf(std::ios::fixed);
+    outf.precision(2);
+
+    outf << std::endl;
+    outf << std::setw(47) << "Employee Pay Table" << std::endl;
+    outf << std::setw(43) << "Fall, 2014" << std::endl;
+    outf << std::endl;
+    outf << std::setw(1) << "Employee" << std::setw(14) << "Employee"
+         << std::setw(10) << "Hours" << std::setw(11) << "Hourly" << std::setw(9) << "Gross"
+         << std::endl;
+    outf << std::setw(5) << "Name" << std::setw(14) << "Type" << std::setw(14)
+         << "Worked" << std::setw(9) << "Rate" << std::setw(11) << "Salary"
+         << std::setw(10) << "Taxes" << std::setw(7) << "Net" << std::endl;
+    for (i = 1; i <= 73; i++)
+        outf << "-";
+    outf << std::endl;
+
+    for (i = 1; i <= 10 && inf >> name >> Emplyt >> hourr >> hoursw; i++)
+    {
+        gross = (hoursw > 40) ? ((hoursw - 40) * (hourr * 1.5) + (40 * hourr)) : (hourr * hoursw);
+        tax = (Emplyt == 'F') ? ((gross < 250) ? 0 : (gross < 400) ? gross * 0.15 : gross * 0.25)
+                              : ((gross >= 250) ? gross * 0.2 : (gross >= 125) ? gross * 0.1 : 0);
+        net = gross - tax;
+        totalhw += hoursw;
+        totaltax += tax;
+        totalnet += net;
+        totalg += gross;
+
+        outf << std::setw(15) << std::left << name << std::setw(10) << Emplyt << std::setw(5) << std::right << hoursw << std::setw(12)
+             << hourr << std::setw(10) << gross << std::setw(10) << tax << std::setw(10) << net << std::endl;
+    }
+
+    for (i = 1; i <= 73; i++)
+        outf << "-";
+    outf << std::endl;
+
+    outf << std::setw(32) << totalhw << std::setw(20) << totalg << std::setw(10) << totaltax << std::setw(11) << totalnet << std::endl;
+
+    std::cin.ignore(); // Wait for user input before closing the console window
+
+    return 0;
+}
+
 			
